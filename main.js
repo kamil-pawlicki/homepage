3e1c58cec163c2d08a47d7e2cc0559d3ea050a28
Kamil Pawlicki <k.l.pawlicki@gmail.com>
2021-04-10 13:57:15 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+alert('siema!');
+
+const firstName = 'Kamil Pawlicki';
+const age = 30;
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} , mam ${age} lat a to jest moja pierwsza strona.`);
